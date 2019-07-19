32219e8c542a0956648d3dc435c3f053978db837
Alexandru Dan <dan.alex97@yahoo.com>
2019-07-19 17:29:49 +0300

    Update speer.sh

--- .tmux.projects/speer.sh
+++ .tmux.projects/speer.sh
@@ -10,24 +10,25 @@ tmux start-server
 # create a new tmux session, starting vim from a saved session in the new window
 tmux new-session -d -s $session -n vim
 
-# Select pane 1, set dir to api, run vim
+# Select pane 1, run vim
 tmux selectp -t 1 
 tmux send-keys "vim" C-m 
 
-# Split pane 1 horizontal by 65%, start redis-server
+# Split pane 1 horizontal by 65%
 tmux splitw -h -p 35
 
-# Select pane 2 
-tmux selectp -t 2
 # Split pane 2 vertiacally by 50%
+tmux selectp -t 2
 tmux splitw -v -p 50
 
-# select pane 3, set to api root
+# select pane 3, run htop
 tmux selectp -t 3
 tmux send-keys "htop" C-m 
 
 # Select pane 1
 tmux selectp -t 1
 
-# Finished setup, attach to the tmux session!
+popd
+
+# attach to the tmux session
 tmux attach-session -t $session
